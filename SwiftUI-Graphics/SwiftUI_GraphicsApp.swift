96153fa2edfbaf5f88b935d5714fe5c594628fd6
Arpit Dixit <arpitd@dewsolutions.in>
2021-07-08 23:09:32 +0530

    Initial Commit

---
+++ SwiftUI-Graphics/SwiftUI_GraphicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GraphicsApp.swift
+//  SwiftUI-Graphics
+//
+//  Created by Arpit Dixit on 08/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GraphicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
